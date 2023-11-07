18bd27282d4ce43251e9cd0f2a3b68689c412c1b
andreaswendlandt <andreaswendlandt.79@googlemail.com>
2023-11-07 20:51:50 +0100

    refactor: fix k8s dns naming convention(remove underlines)

--- cronguard/daemon/db.inc.sh
+++ cronguard/daemon/db.inc.sh
@@ -1,5 +1,5 @@
 # Database Credentials and Values for the Cronguard Daemon
-db_host=cronguard_db
+db_host=cronguard-db
 db_user=cronguard
 db_password=top_secret
 db=cronguard
--- cronguard/webserver/db.inc.php
+++ cronguard/webserver/db.inc.php
@@ -1,6 +1,6 @@
 <?php
 // Connecting to the database with the following credentials("host","user","password","database")
-$conn = @(new mysqli("cronguard_db", "cronguard", "top_secret", "cronguard"));
+$conn = @(new mysqli("cronguard-db", "cronguard", "top_secret", "cronguard"));
 if ($conn->connect_error) {
   echo "Error while connecting: " . mysqli_connect_error();
   exit();
